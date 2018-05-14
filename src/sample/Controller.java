3994891a9367e77612e3025ce8c0a8818436e257
DESKTOP-P3VVJDG\maikb <maik.bialas@freenet.de>
2018-05-14 06:02:36 +0200

    initial

---
+++ src/sample/Controller.java
@@ -0,0 +1,24 @@
+package sample;
+
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    Button s;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("Hello");
+    }
+
+
+
+
+}
